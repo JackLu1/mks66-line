a80d85b2e4020ea6272d5e0bd3787a4080d3ddc4
Jack Lu <jlu9@stuy.edu>
2019-02-06 15:19:47 -0500

    1

---
+++ main.py
@@ -0,0 +1,12 @@
+from display import *
+from draw import *
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+#draw_line(0,0,200,200,screen,[255,255,255])
+for i in range(200):
+    plot(screen, color, i, i)
+    plot(screen, color, i, 20+i)
+
+display(screen)
+save_extension(screen, 'img.png')
