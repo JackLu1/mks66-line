a01e2775ef9ba333e38e785905ebbae421a59211
Jack Lu <jlu9@stuy.edu>
2019-02-06 15:39:04 -0500

    OOF

--- draw.py
+++ draw.py
@@ -1,4 +1,9 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+    '''OOF'''
+    x = x0
+    y = y0
+    x += 1
+
+
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-#draw_line(0,0,200,200,screen,[255,255,255])
-for i in range(200):
-    plot(screen, color, i, i)
-    plot(screen, color, i, 20+i)
+# draw_line(0,0,200,200,screen,[255,255,255])
+# for i in range(200):
+#     plot(screen, color, i, i)
+#     plot(screen, color, i, 20+i)
 
 display(screen)
 save_extension(screen, 'img.png')
