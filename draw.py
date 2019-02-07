dae5c71b9bb3521120386b7afb0b82538459246c
Jack Lu <jlu9@stuy.edu>
2019-02-07 12:02:40 -0500

    .

--- draw.py
+++ draw.py
@@ -1,9 +1,10 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    '''OOF'''
     x = x0
     y = y0
-    x += 1
-
+    A = y1 - y0
+    B = -1 * (x1 - x0)
+    while x <= x1:
+        plot( screen, color, x, y )
 
