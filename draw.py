419c03a2359829928b757cecfaa4c303fb11012a
Jack Lu <jlu9@stuy.edu>
2019-02-08 15:33:44 -0500

    aaa

--- draw.py
+++ draw.py
@@ -5,7 +5,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
     y = y0
     A = y1 - y0
     B = -1 * (x1 - x0)
-    d = 2 * A - B
+    d = 2 * A + B
     while x <= x1:
         plot( screen, color, x, y )
         if d > 0:
--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-draw_line(0,0,500,10,screen,[255,255,255])
-draw_line(0,0,250,50,screen,[255,255,255])
-draw_line(100,100,250,159,screen,[255,255,255])
-draw_line(00,100,250,159,screen,[255,255,255])
+i = 250
+while i < 500:
+    draw_line(250,250,500,i,screen,[255,255,255])
+    i += 10
+draw_line(250,250,500,0,screen,[255,255,255])
+#draw_line(250,250,500,350,screen,[255,255,255])
+#draw_line(250,250,500,250,screen,[255,255,255])
+#draw_line(250,250,500,-100,screen,[255,255,255])
+
+#draw_line(0,0,250,50,screen,[255,255,255])
+#draw_line(100,100,250,159,screen,[0,0,255])
+#draw_line(00,100,250,159,screen,[0,255,0])
 # for i in range(200):
 #     plot(screen, color, i, i)
 #     plot(screen, color, i, 20+i)
