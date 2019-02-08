57d7a85e66db7feb7e58aefa8e513240c9ec5a7b
Jack Lu <jlu9@stuy.edu>
2019-02-08 13:58:57 -0500

    !

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-# draw_line(0,0,200,200,screen,[255,255,255])
+draw_line(0,0,500,10,screen,[255,255,255])
+draw_line(0,0,250,50,screen,[255,255,255])
+draw_line(100,100,250,159,screen,[255,255,255])
+draw_line(00,100,250,159,screen,[255,255,255])
 # for i in range(200):
 #     plot(screen, color, i, i)
 #     plot(screen, color, i, 20+i)
