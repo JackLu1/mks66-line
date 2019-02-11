c42c3b5deb89ea5805eae8be3bb1f1a609c9bc0f
Jack Lu <jlu2025@gmail.com>
2019-02-11 03:01:40 -0500

    k

--- draw.py
+++ draw.py
@@ -1,15 +1,172 @@
 from display import *
 
+# oct 1
 def draw_line( x0, y0, x1, y1, screen, color ):
     x = x0
     y = y0
     A = y1 - y0
     B = -1 * (x1 - x0)
-    d = 2 * A + B
+    dy = y1 - y0
+    dx = x1 - x0
+    print(dy)
+    print(dx)
+
+
+    # oct 1
+    if dy >= 0 and dx >= 0 and dy <= dx:
+        print("OUCT1")
+        d = line(A, B)
+        while x <= x1:
+            plot( screen, color, x, y )
+            if d > 0:
+                y += 1
+                d += 2*B
+            x += 1
+            d += 2*A
+
+    # oct 2
+    if dy >= 0 and dx >= 0 and dy >= dx:
+        print("OUCT2")
+        d = line(B, A)
+        while y <= y1:
+            plot( screen, color, x, y )
+            if d < 0:
+                x += 1
+                d += 2*A
+            y += 1
+            d += 2*B
+
+    # oct 3
+    if dy >= 0 and dx <= 0 and dy >= abs(dx):
+        print("OUCT3")
+        B = x1 - x0
+        d = line(B, A)
+        while y <= y1:
+            plot( screen, color, x, y )
+            if d < 0:
+                x -= 1
+                d += 2*A
+            y += 1
+            d += 2*B
+    # oct 4
+    if dy >= 0 and dx <= 0 and dy <= abs(dx):
+        print("OUCT4")
+        B = x1 - x0
+        d = line(A, B)
+        while x >= x1:
+            plot( screen, color, x, y )
+            if d > 0:
+                y += 1
+                d += 2*B
+            x -= 1
+            d += 2*A
+    # oct 5
+    # oct 6
+    # oct 7
+    else:
+        print("##")
+
+    # oct 8
+
+
+# oct 2
+def drawline( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = -1 * (x1 - x0)
+    d = line(B, A)
+    while y <= y1:
+        plot( screen, color, x, y )
+        if d < 0:
+            x += 1
+            d += 2*A
+        y += 1
+        d += 2*B
+
+def draw_oct7( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = -1 * (x1 - x0)
+    d = line(B, -1*A)
+    while y >= y1:
+        plot( screen, color, x, y )
+        if d < 0:
+            x += 1
+            d -= 2*A
+        y -= 1
+        d += 2*B
+
+def draw_oct8( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y0 - y1
+    B = -1 * (x1 - x0)
+    d = line(A, B)
     while x <= x1:
         plot( screen, color, x, y )
         if d > 0:
-            y += 1
-            d += B
+            y -= 1
+            d += 2*B
         x += 1
         d += 2*A
+
+def draw_oct3( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x1 - x0
+    d = line(B, A)
+    while y <= y1:
+        plot( screen, color, x, y )
+        if d < 0:
+            x -= 1
+            d += 2*A
+        y += 1
+        d += 2*B
+
+def draw_oct4( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x1 - x0
+    d = line(A, B)
+    while x >= x1:
+        plot( screen, color, x, y )
+        if d > 0:
+            y += 1
+            d += 2*B
+        x -= 1
+        d += 2*A
+
+def draw_oct5( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y0 - y1
+    B = x1 - x0
+    d = line(A, B)
+    while x >= x1:
+        plot( screen, color, x, y )
+        if d > 0:
+            y -= 1
+            d += 2*B
+        x -= 1
+        d += 2*A
+
+def draw_oct6( x0, y0, x1, y1, screen, color ):
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = x1 - x0
+    d = line(B, -1*A)
+    while y >= y1:
+        plot( screen, color, x, y )
+        if d < 0:
+            x -= 1
+            d -= 2*A
+        y -= 1
+        d += 2*B
+
+def line(x, y):
+    return 2 * x + y
--- main.py
+++ main.py
@@ -3,21 +3,58 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-i = 250
-while i < 500:
-    draw_line(250,250,500,i,screen,[255,255,255])
-    i += 10
-draw_line(250,250,500,0,screen,[255,255,255])
-#draw_line(250,250,500,350,screen,[255,255,255])
-#draw_line(250,250,500,250,screen,[255,255,255])
-#draw_line(250,250,500,-100,screen,[255,255,255])
 
-#draw_line(0,0,250,50,screen,[255,255,255])
-#draw_line(100,100,250,159,screen,[0,0,255])
-#draw_line(00,100,250,159,screen,[0,255,0])
-# for i in range(200):
-#     plot(screen, color, i, i)
-#     plot(screen, color, i, 20+i)
+#i = 0
+#while i < 250:
+#    draw_oct3(250,250,i,500,screen,[255,255,255])
+#    i += 25
+
+# oct 3
+draw_line(250,250,150,500,screen,color)
+# oct 4
+draw_line(250,250,0,500,screen,color)
+draw_line(250,250,0,270,screen,color)
+
+# oct 1
+draw_line(250,250,500,250,screen,color)
+draw_line(250,250,500,500,screen,color)
+
+# oct 2
+draw_line(250,250,300,500,screen,[255,255,255])
+draw_line(250,250,359,500,screen,[255,255,255])
+draw_line(250,250,250,500,screen,[255,255,255])
+
+# oct 3
+#draw_oct3(250,250,200,500,screen,color)
+#draw_oct3(250,250,250,500,screen,color)
+#draw_oct3(250,250,0,500,screen,color)
+
+# oct 4
+#draw_oct4(250,250,0,300,screen,color)
+#draw_oct4(250,250,0,500,screen,color)
+#draw_oct4(250,250,0,250,screen,color)
+
+# oct 5
+#draw_oct5(250,250,0,200,screen,color)
+#draw_oct5(250,250,0,0,screen,color)
+#draw_oct5(250,250,0,250,screen,color)
+
+# oct 6
+#draw_oct6(250,250,0,0,screen,color)
+#draw_oct6(250,250,250,0,screen,color)
+#draw_oct6(250,250,100,0,screen,color)
+
+# oct 7
+#draw_oct7(250,250,300,0,screen,color)
+#draw_oct7(250,250,400,0,screen,color)
+#draw_oct7(250,250,250,0,screen,color)
+
+# oct 8
+#draw_oct8(250,250,500,200,screen,color)
+#draw_oct8(250,250,500,250,screen,color)
+#draw_oct8(250,250,500,0,screen,color)
+#draw_oct8(250,250,500,125,screen,color)
+
 
 display(screen)
 save_extension(screen, 'img.png')
